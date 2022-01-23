ea6e58684623831bf83972e9c5526fcc929e4dd2
Chiagoziem James <66981472+James-Leonard@users.noreply.github.com>
2022-01-23 03:35:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function compute()
 
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate / 1000;
+    var interest = principal * years * rate / 100;
 
     var dateNow = new Date();
     var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
